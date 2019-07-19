d288ddc3b82331dc2f9df1191d456e35070f9987
HongJow <hoongjow@gmail.com>
2019-07-19 13:09:36 +0900

    stop on the way

--- src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
+++ src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
@@ -7,14 +7,24 @@ import org.junit.Test;
 public class DirtySampleTest {
 
     @Test
-    public void updateQuality_잡다한_아이템이고_quality_sellin이_충분한_경우_(){
+    public void updateQuality_잡다한_아이템이고_quality_sellIn이_충분한_경우_(){
         Item[] items = new Item[1];
         items[0] = new Item("rion king", 5, 5);
         DirtySample dirtySample = new DirtySample(items);
-
         dirtySample.updateQuality();
+
         assertThat(items[0].quality, is(4));
         assertThat(items[0].sellIn, is(4));
+    }
+
+    @Test
+    public void updateQuality_잡다한아이템_sellIn_is_0(){
+        Item[] items = new Item[1];
+        items[0] = new Item("rion king", 0, 5);
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+
     }
 
 }
