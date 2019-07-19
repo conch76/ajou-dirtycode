ad55eeca08d60c4973051b606c3a124bf2a32429
HongJow <hoongjow@gmail.com>
2019-07-19 12:16:23 +0900

    testcode for normal case

---
+++ src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
@@ -0,0 +1,20 @@
+package ac.kr.ajou.dirt;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import org.junit.Test;
+
+public class DirtySampleTest {
+
+    @Test
+    public void updateQuality_잡다한_아이템이고_quality_sellin이_충분한_경우_(){
+        Item[] items = new Item[1];
+        items[0] = new Item("rion king", 5, 5);
+        DirtySample dirtySample = new DirtySample(items);
+
+        dirtySample.updateQuality();
+        assertThat(items[0].quality, is(4));
+        assertThat(items[0].sellIn, is(4));
+    }
+
+}
