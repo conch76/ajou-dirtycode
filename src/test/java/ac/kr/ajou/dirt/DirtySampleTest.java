35b34b28b1aef8580f00d387ce4c3d1620c49fee
HongJow <hoongjow@gmail.com>
2019-07-20 17:08:16 +0900

    add testcode for Aged Brie item

--- src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
+++ src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
@@ -7,24 +7,52 @@ import org.junit.Test;
 public class DirtySampleTest {
 
     @Test
-    public void updateQuality_잡다한_아이템이고_quality_sellIn이_충분한_경우_(){
+    public void updateQuality_stuffs_enough_sellIn(){
+        //quality, sellIn: - 1
         Item[] items = new Item[1];
-        items[0] = new Item("rion king", 5, 5);
+        items[0] = new Item("frostmourne", 5, 5);
         DirtySample dirtySample = new DirtySample(items);
-        dirtySample.updateQuality();
+        dirtySample.update_quality();
 
         assertThat(items[0].quality, is(4));
         assertThat(items[0].sellIn, is(4));
     }
 
     @Test
-    public void updateQuality_잡다한아이템_sellIn_is_0(){
+    public void update_quality_stuffs_has_negative_sellIn(){
+        //quality: - 2 , sellIn: - 1
         Item[] items = new Item[1];
-        items[0] = new Item("rion king", 0, 5);
+        items[0] = new Item("frostmourne", -3, 5);
         DirtySample dirtySample = new DirtySample(items);
-        dirtySample.updateQuality();
+        dirtySample.update_quality();
 
+        assertThat(items[0].quality, is(3));
+        assertThat(items[0].sellIn, is(-4));
 
+    }
+
+    @Test
+    public void update_quality_Aged_Brie_is_has_enough_sellIn_and_quality_less_than_50(){
+        //quality: + 1, sellIn: - 1
+        Item[] items = new Item[1];
+        items[0] = new Item("Aged Brie", 5, 5);
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.update_quality();
+
+        assertThat(items[0].quality, is(6));
+        assertThat(items[0].sellIn, is(4));
+    }
+
+    @Test
+    public void update_quality_Aged_Brie_is_has_enough_sellIn_and_quality_more_than_50(){
+        //quality: + 1, sellIn: - 1
+        Item[] items = new Item[1];
+        items[0] = new Item("Aged Brie", 5, 55);
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.update_quality();
+
+        assertThat(items[0].quality, is(55));
+        assertThat(items[0].sellIn, is(4));
     }
 
 }
