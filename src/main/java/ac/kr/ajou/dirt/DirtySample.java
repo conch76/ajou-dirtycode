d0be13f353c37ce38eea31ecf7e8cd805e14669f
Sujin <sujin.k0402@gmail.com>
2019-07-22 14:55:33 +0900

    if 순서 바꿔서 이름에 따라 if, else if, else 분리

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -35,21 +35,21 @@ class DirtySample {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
                     }
+                } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    item.quality = item.quality - item.quality;
+                } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
                 } else {
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        item.quality = item.quality - item.quality;
-                    } else {
-                        if (item.quality > 0) {
-                            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                continue;
-                            }
-                            item.quality = item.quality - 1;
-                        }
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
                     }
                 }
+
             }
+
         }
     }
+
+}
 
     private void increaseOneItemQuality(Item item) {
         if (item.quality < 50) {
