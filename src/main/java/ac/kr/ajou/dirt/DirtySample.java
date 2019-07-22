1a2d96797c247b54158d900f854c6681b41f761d
Sujin <sujin.k0402@gmail.com>
2019-07-22 15:04:28 +0900

    두 if문 합치고 줄 정리

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -3,55 +3,33 @@ package ac.kr.ajou.dirt;
 class DirtySample {
     Item[] items;
 
-    public DirtySample(Item[] items) {
-        this.items = items;
-    }
+    public DirtySample(Item[] items) { this.items = items; }
 
     public void updateQuality() {
         for (Item item : items) {
-
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn -= 1;
-            }
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) { item.sellIn -= 1; }
 
             if (item.name.equals("Aged Brie")) {
                 increaseOneItemQuality(item);
-                if (item.sellIn < 0) {
-                    increaseOneItemQuality(item);
-                }
+                if (item.sellIn < 0) { increaseOneItemQuality(item); }
             } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 increaseOneItemQuality(item);
-                if (item.sellIn < 11) {
-                    increaseOneItemQuality(item);
-                }
-
-                if (item.sellIn < 6) {
-                    increaseOneItemQuality(item);
-                }
-                if (item.sellIn < 0) {
-                    item.quality = 0;
-                }
+                if (item.sellIn < 11) { increaseOneItemQuality(item); }
+                if (item.sellIn < 6) { increaseOneItemQuality(item); }
+                if (item.sellIn < 0) { item.quality = 0; }
             } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-
             } else {
                 decreaseOneItemQuality(item);
-                if (item.sellIn < 0) {
-                    decreaseOneItemQuality(item);
-
-                }
+                if (item.sellIn < 0) { decreaseOneItemQuality(item); }
             }
         }
     }
 
     private void increaseOneItemQuality(Item item) {
-        if (item.quality < 50) {
-            item.quality += 1;
-        }
+        if (item.quality < 50) { item.quality += 1; }
     }
 
     private void decreaseOneItemQuality(Item item) {
-        if (item.quality > 0) {
-            item.quality -= 1;
-        }
+        if (item.quality > 0) { item.quality -= 1; }
     }
 }
