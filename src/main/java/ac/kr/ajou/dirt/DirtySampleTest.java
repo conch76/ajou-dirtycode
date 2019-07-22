0ac728baed1079aa99cd5ce4022b9dc72812eabe
Jsnackash <jbjeon0113@naver.com>
2019-07-22 11:55:09 +0900

    updateQuality item의 sellIn이 0미만이고 name이 Aged와 Backstage와 Sulfuras가 아니고 quality가 0보다 크면 quality가 2 감소하는 테스트

--- src/main/java/ac/kr/ajou/dirt/DirtySampleTest.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySampleTest.java
@@ -2,17 +2,26 @@ package ac.kr.ajou.dirt;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class DirtySampleTest {
 
     @Test
     public void updateQuality_item의_name이_Sulfuras가_아니면_sellIn_1_감소() {
-        Item[] items = new Item[] { new Item("foo", 2, 2) };
+        Item[] items = new Item[]{new Item("foo", 2, 2)};
         DirtySample dirtySample = new DirtySample(items);
         dirtySample.updateQuality();
 
         assertTrue(items[0].sellIn == 1);
     }
 
+    @Test
+    public void updateQuality_item의_sellIn이_0미만이고_name이_Aged와_Backstage와_Sulfuras가_아니고_quality가_0보다_크면_quality_2_감소() {
+        Item[] items = new Item[]{new Item("foo", -2, 3)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].quality, is(1));
+    }
 }
