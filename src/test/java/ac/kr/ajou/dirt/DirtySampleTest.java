12cd3ab70127af74461b628427c5c598d0ee490b
rkdcksgur <rkdcksgur@ajou.ac.kr>
2019-07-23 12:42:37 +0900

    import org.junit.test

--- src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
+++ src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
@@ -1,6 +1,17 @@
 package ac.kr.ajou.dirt;
 
+import org.junit.Test;
+
 public class DirtySampleTest {
 
+    Item[] items;
+
+    @Test
+    public void someTest(){
+        DirtySample dirtySample = new DirtySample(items);
+
+
+
+    }
 
 }
