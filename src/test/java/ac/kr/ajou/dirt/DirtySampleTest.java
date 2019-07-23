46c5586ba2aba698fd10e75e8d5bf76cf5c2d280
rkdcksgur <rkdcksgur@ajou.ac.kr>
2019-07-23 13:04:40 +0900

    add test example

--- src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
+++ src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
@@ -2,15 +2,18 @@ package ac.kr.ajou.dirt;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+
 public class DirtySampleTest {
 
-    Item[] items;
-
     @Test
-    public void someTest(){
+    public void AgedBrie아니고NOBackstagePasses아니고Sulfuras아닐때Test(){
+        Item[] items = {new Item("aaa", 0, 1)};
         DirtySample dirtySample = new DirtySample(items);
-
-
+        dirtySample.updateQuality();
+        assertThat(items[0].quality, is(0));
 
     }
 
