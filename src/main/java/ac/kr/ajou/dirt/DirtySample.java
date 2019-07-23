b8c1952a798dfad1b59f5edf2d28716fd858b3e0
kimdohyun <kdh5998@ajou.ac.kr>
2019-07-23 18:26:28 +0900

    updateQuality메소드 안에 if문들을 좀 나눠서 여러 메소드로 분리해봤습니다.

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -1,62 +1,98 @@
 package ac.kr.ajou.dirt;
 
-class DirtySample {
+public class DirtySample {
     Item[] items;
 
     public DirtySample(Item[] items) {
         this.items = items;
     }
 
+    //코드 의도 파악 -> 테스트를 만들어라 -> refactoring해라 -> refactoring 과정 작성
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+            if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                Quality가0보다크고_Sulfuras가아니라면_퀄리티1감소(items, i);
             }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            else {
+                Quality가50보다작으면_퀄리티1증가후_Backstage판단(items, i);
             }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+            Sulfras가아니면_SellIn이1감소(items, i);
+            SellIn이_0이하라면(items, i);
         }
     }
+
+
+    public static void Quality가0보다크고_Sulfuras가아니라면_퀄리티1감소(Item[] items, int count) {
+        if (items[count].quality > 0 && !items[count].name.equals("Sulfuras, Hand of Ragnaros")) {
+            items[count].quality = items[count].quality - 1;
+        }
+    }
+
+
+    public static void Quality가50보다작으면_퀄리티1증가후_Backstage판단(Item[] items, int count) {
+        if (items[count].quality < 50) {
+            items[count].quality = items[count].quality + 1;
+
+            Backstage판단(items, count);
+        }
+    }
+
+    public static void Backstage판단(Item[] items, int count) {
+        if (items[count].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            Quality와SellIn에_따라_퀄리티1증가(items, count);
+        }
+    }
+
+    public static void Quality와SellIn에_따라_퀄리티1증가(Item[] items, int count) {
+        if (items[count].sellIn < 11 && items[count].quality < 50) {
+            items[count].quality = items[count].quality + 1;
+        }
+        if (items[count].sellIn < 6 && items[count].quality < 50) {
+            items[count].quality = items[count].quality + 1;
+        }
+    }
+
+    public static void SellIn이_0이하라면(Item[] items, int count){
+        if (items[count].sellIn < 0) {
+            AgedBrie_인지아닌지(items, count);
+        }
+    }
+
+    public static void AgedBrie_인지아닌지(Item[] items, int count) {
+        if (!items[count].name.equals("Aged Brie")) {
+            AgedBrie인지에따라_Quality변화(items, count);
+        }
+        else {
+            Quality가50보다작으면_Quality1증가(items, count);
+        }
+    }
+
+    public static void AgedBrie인지에따라_Quality변화(Item[] items, int count) {
+        if (!items[count].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            Sulfras가아니고_Quality가0보다크면_Quality1감소(items, count);
+        }
+        else {
+            items[count].quality = 0;
+        }
+    }
+
+    public static void Sulfras가아니고_Quality가0보다크면_Quality1감소(Item[] items, int count){
+        if (items[count].quality > 0 && !items[count].name.equals("Sulfuras, Hand of Ragnaros")) {
+            items[count].quality = items[count].quality - 1;
+        }
+    }
+
+    public static void Quality가50보다작으면_Quality1증가(Item[] items, int count) {
+        if (items[count].quality < 50) {
+            items[count].quality = items[count].quality + 1;
+        }
+    }
+
+    public static void Sulfras가아니면_SellIn이1감소(Item[] items, int count){
+        if (!items[count].name.equals("Sulfuras, Hand of Ragnaros")) {
+            items[count].sellIn = items[count].sellIn - 1;
+        }
+    }
+
+
 }
