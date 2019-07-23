28265f0544a88a303ed0771fd926e1e21da3b063
JooJaeLin <jl1096@ajou.ac.kr>
2019-07-24 00:57:29 +0900

    이름이 AGED_BRIE이고 퀄리티가 50보다 작으면 퀄리티는 1만큼 증가하는 TEST CODE

--- src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
+++ src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
@@ -23,6 +23,14 @@ public class DirtySampleTest {
         assertThat(items[0].quality,is(0));
     }
 
+    //이름이 AGED_BRIE이고 퀄리티가 50보다 작으면 퀄리티는 1만큼 증가해야한다.
+    @Test
+    public void 이름이_AgedBrie이고_퀄리티는_40이면_퀄리티는_41이어야함(){
+        Item[] items = {new Item(AGED_BRIE,10,40)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+        assertThat(items[0].quality, is(41));
+    }
 
     @Test
     public void doSomething_이름이셋다아니고_sellIn이음수이고_quailty가음수일때() {
