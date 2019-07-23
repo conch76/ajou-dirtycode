de7cb3edef8bb422d2a735c32ea58d4e32d0953a
doroddong <dongpang1014@ajou.ac.kr>
2019-07-23 18:00:32 +0900

    Add test when item[0]'s name is "Sulfuras, Hand of Ragnaros"

--- src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
+++ src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
@@ -11,6 +11,7 @@ public class DirtySampleTest {
     public static final String NONE_OF_THREE_NAMES = "Refactoring!!";
     public static final String AGED_BRIE = "Aged Brie";
     public static final String TAFKAL80ETC = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
     @Test
     public void doSomething_이름이셋다아니고_sellIn이음수이고_quailty가음수일때() {
@@ -367,6 +368,125 @@ public class DirtySampleTest {
         dirtySample.updateQuality();
 
         assertThat(items[0].sellIn, is(12));
+        assertThat(items[0].quality, is(60));
+    }
+
+    @Test
+    public void doSomething_이름이SULFURAS이고_sellIn이음수이고_quailty가음수일때() {
+        Item[] items = {new Item(SULFURAS, -5, -5)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(-5));
+        assertThat(items[0].quality, is(-5));
+    }
+
+    @Test
+    public void doSomething_이름이SULFURAS이고_sellIn이음수이고_quailty가25일때() {
+        Item[] items = {new Item(SULFURAS, -5, 25)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(-5));
+        assertThat(items[0].quality, is(25));
+    }
+
+    @Test
+    public void doSomething_이름이SULFURAS이고_sellIn이음수이고_quailty가60일때() {
+        Item[] items = {new Item(SULFURAS, -5, 60)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(-5));
+        assertThat(items[0].quality, is(60));
+    }
+    @Test
+    public void doSomething_이름이SULFURAS이고_sellIn이3이고_quailty가음수일때() {
+        Item[] items = {new Item(SULFURAS, 3, -5)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(3));
+        assertThat(items[0].quality, is(-5));
+    }
+
+    @Test
+    public void doSomething_이름이SULFURAS이고_sellIn이3이고_quailty가25일때() {
+        Item[] items = {new Item(SULFURAS, 3, 25)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(3));
+        assertThat(items[0].quality, is(25));
+    }
+
+    @Test
+    public void doSomething_이름이SULFURAS이고_sellIn이3이고_quailty가60일때() {
+        Item[] items = {new Item(SULFURAS, 3, 60)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(3));
+        assertThat(items[0].quality, is(60));
+    }
+
+    @Test
+    public void doSomething_이름이SULFURAS이고_sellIn이8이고_quailty가음수일때() {
+        Item[] items = {new Item(SULFURAS, 8, -5)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(8));
+        assertThat(items[0].quality, is(-5));
+    }
+
+    @Test
+    public void doSomething_이름이SULFURAS이고_sellIn이8이고_quailty가25일때() {
+        Item[] items = {new Item(SULFURAS, 8, 25)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(8));
+        assertThat(items[0].quality, is(25));
+    }
+
+    @Test
+    public void doSomething_이름이SULFURAS이고_sellIn이8이고_quailty가60일때() {
+        Item[] items = {new Item(SULFURAS, 8, 60)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(8));
+        assertThat(items[0].quality, is(60));
+    }
+
+    @Test
+    public void doSomething_이름이SULFURAS이고_sellIn이13이고_quailty가음수일때() {
+        Item[] items = {new Item(SULFURAS, 13, -5)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(13));
+        assertThat(items[0].quality, is(-5));
+    }
+
+    @Test
+    public void doSomething_이름이SULFURAS이고_sellIn이13이고_quailty가25일때() {
+        Item[] items = {new Item(SULFURAS, 13, 25)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(13));
+        assertThat(items[0].quality, is(25));
+    }
+
+    @Test
+    public void doSomething_이름이SULFURAS이고sellIn이13이고_quailty가60일때() {
+        Item[] items = {new Item(SULFURAS, 13, 60)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(13));
         assertThat(items[0].quality, is(60));
     }
 }
