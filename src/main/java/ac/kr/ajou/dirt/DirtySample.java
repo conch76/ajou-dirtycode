eeb33d37cb7afcd83ecf36b60abe3fa681f8dc7d
Chiwan <cldhks@ajou.ac.kr>
2019-07-23 21:06:34 +0900

    Fix Some Method's name and Refactoring duplicate code (Make Method for them) delete unneccessery code

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -15,7 +15,7 @@ class DirtySample {
             NameIsNotSulfuras(i);
             if (items[i].sellIn < 0) {
                 ItemNameIsNotAllOfTheCaseAndQualityOverZero(i);
-                ItemNameIsBackageTButNotAgedBrie(i);
+                ItemNameIsBackage(i);
                 NameisAgedBrie(i);
             }
         }
@@ -26,7 +26,7 @@ class DirtySample {
             if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+                        QualityOfItemMinusOne(i);
                     }
                 }
             }
@@ -34,38 +34,33 @@ class DirtySample {
 
     }
 
-    private void ItemNameIsBackageTButNotAgedBrie(int i) {
-        if (!items[i].name.equals("Aged Brie")) {
-
-            if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                items[i].quality = items[i].quality - items[i].quality;
-            }
-        }
+    private void QualityOfItemMinusOne(int i) {
+        items[i].quality = items[i].quality - 1;
     }
 
+    private void ItemNameIsBackage(int i) {
+           if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                items[i].quality = 0;
+           }
+    }
     private void NameisAgedBrie(int i) {
         if (items[i].name.equals("Aged Brie")) {
             if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1;
+                QualityOfItemPlusOne(i);
             }
         }
 
     }
-    private void NameIsNotAgedBrieNotBackageT(int i){//중복코드
-        if (!items[i].name.equals("Aged Brie")
-                && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items[i].quality > 0) {
-                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                    items[i].quality = items[i].quality - 1;
-                }
-            }
-        }
+
+    private void QualityOfItemPlusOne(int i) {
+        items[i].quality = items[i].quality + 1;
     }
+
     private void NameIsAgedOrBackageAndQualityLessThan50(int i) {
         if (!((!items[i].name.equals("Aged Brie")
                 && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")))) {
             if (items[i].quality < 50)
-                items[i].quality = items[i].quality + 1;
+                QualityOfItemPlusOne(i);
         }
     }
     private void NameIsBackageAndQualityLessThan50(int i){
@@ -73,14 +68,16 @@ class DirtySample {
             if(items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")){
                 if (items[i].quality < 50){
                     if (items[i].sellIn < 11)
-                        items[i].quality = items[i].quality + 1;
+                        QualityOfItemPlusOne(i);
                     if(items[i].sellIn < 6)
-                        items[i].quality = items[i].quality + 1;
+                        QualityOfItemPlusOne(i);
                 }
             }
     }
     private void NameIsNotSulfuras(int i){
-        items[i].sellIn = items[i].sellIn - 1;
+        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            items[i].sellIn = items[i].sellIn - 1;
+        }
     }
 
 
