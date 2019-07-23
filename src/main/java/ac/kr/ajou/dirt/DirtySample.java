6a922f7e0db58abfb1e0dcb9688243a79f84def0
rkdcksgur <rkdcksgur@ajou.ac.kr>
2019-07-23 22:25:42 +0900

    Aged || Backstage if문을 둘로 쪼개기

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -11,7 +11,7 @@ public class DirtySample {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
 
-            if ( items[i].name.equals("Aged Brie") || items[i].name.equals("Backstage passes to a TAFKAL80ETC concert") ) { //aged or backstage
+            if ( items[i].name.equals("Aged Brie") ) { //aged
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
@@ -22,6 +22,17 @@ public class DirtySample {
                         if (items[i].sellIn < 6 && items[i].quality < 50) {
                             items[i].quality = items[i].quality + 1;
                         }
+                    }
+                }
+            }
+            else if( items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")){ //backstage
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+                    if (items[i].sellIn < 11 && items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+                    }
+                    if (items[i].sellIn < 6 && items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
                     }
                 }
             }
