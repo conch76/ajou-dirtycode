3100aba419810c9d9220984d9951888a721d72b8
rkdcksgur <rkdcksgur@ajou.ac.kr>
2019-07-23 22:54:27 +0900

    organizing with extract plus and minus methods

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -13,42 +13,52 @@ public class DirtySample {
 
             if(items[i].name.equals("Sulfuras, Hand of Ragnaros")) //sulfuras
                 continue;
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) //sulfuras가 아니라면
-                items[i].sellIn = items[i].sellIn - 1;
-            //second if
+
+            MinusItemSellIn(i);
 
             if ( items[i].name.equals("Aged Brie") ) { //aged
-                if (items[i].quality < 50)
-                    items[i].quality = items[i].quality + 1;
-                if (items[i].sellIn < 0)
-                    if (items[i].quality < 50)
-                        items[i].quality = items[i].quality + 1;
+                if (items[i].quality < 50) {
+                    PlusItemQuality(i);
+                }
+                if (items[i].sellIn < 0 && items[i].quality < 50)
+                    PlusItemQuality(i);
             }
             else if( items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")){ //backstage
                 if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                    PlusItemQuality(i);
                     if (items[i].sellIn < 11 && items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                        PlusItemQuality(i);
                     }
                     if (items[i].sellIn < 6 && items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                        PlusItemQuality(i);
                     }
                 }
                 if (items[i].sellIn < 0)
                     items[i].quality = 0;
             }
             else{
-                if (items[i].quality > 0 )
-                    items[i].quality = items[i].quality - 1;
-                if (items[i].sellIn < 0)
-                    if (items[i].quality > 0)
-                        items[i].quality = items[i].quality - 1;
+                if (items[i].quality > 0 ) {
+                    MinusItemQuality(i);
+                }
+                if (items[i].sellIn < 0 && items[i].quality > 0)
+                    MinusItemQuality(i);
             } //first if
 
         }//for
 
     }
 
+    private void MinusItemSellIn(int i) {
+        items[i].sellIn = items[i].sellIn - 1;
+    }
+
+    private void MinusItemQuality(int i) {
+        items[i].quality = items[i].quality - 1;
+    }
+
+    private void PlusItemQuality(int i) {
+        items[i].quality = items[i].quality + 1;
+    }
 
 
 }
