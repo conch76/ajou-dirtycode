1295650483a54601874dce7bb3a4933afa325eb8
doroddong <dongpang1014@ajou.ac.kr>
2019-07-23 17:55:22 +0900

    Add Unit_test when item[0]'s name is not same any of three names

--- src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
+++ src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
@@ -8,13 +8,125 @@ import static org.junit.Assert.*;
 
 public class DirtySampleTest {
 
+    public static final String NONE_OF_THREE_NAMES = "Refactoring!!";
+
     @Test
-    public void AgedBrie아니고NOBackstagePasses아니고Sulfuras아닐때Test(){
-        Item[] items = {new Item("aaa", 0, 1)};
+    public void doSomething_이름이셋다아니고_sellIn이음수이고_quailty가음수일때() {
+        Item[] items = {new Item(NONE_OF_THREE_NAMES, -5, -5)};
         DirtySample dirtySample = new DirtySample(items);
         dirtySample.updateQuality();
-        assertThat(items[0].quality, is(0));
 
+        assertThat(items[0].sellIn, is(-6));
+        assertThat(items[0].quality, is(-5));
     }
 
+    @Test
+    public void doSomething_이름이셋다아니고_sellIn이음수이고_quailty가25일때() {
+        Item[] items = {new Item(NONE_OF_THREE_NAMES, -5, 25)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(-6));
+        assertThat(items[0].quality, is(23));
+    }
+
+    @Test
+    public void doSomething_이름이셋다아니고_sellIn이음수이고_quailty가60일때() {
+        Item[] items = {new Item(NONE_OF_THREE_NAMES, -5, 60)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(-6));
+        assertThat(items[0].quality, is(58));
+    }
+
+    @Test
+    public void doSomething_이름이셋다아니고_sellIn이3이고_quailty가음수일때() {
+        Item[] items = {new Item(NONE_OF_THREE_NAMES, 3, -5)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(2));
+        assertThat(items[0].quality, is(-5));
+    }
+
+    @Test
+    public void doSomething_이름이셋다아니고_sellIn이3이고_quailty가25일때() {
+        Item[] items = {new Item(NONE_OF_THREE_NAMES, 3, 25)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(2));
+        assertThat(items[0].quality, is(24));
+    }
+
+    @Test
+    public void doSomething_이름이셋다아니고_sellIn이3이고_quailty가60일때() {
+        Item[] items = {new Item(NONE_OF_THREE_NAMES, 3, 60)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(2));
+        assertThat(items[0].quality, is(59));
+    }
+
+    @Test
+    public void doSomething_이름이셋다아니고_sellIn이8이고_quailty가음수일때() {
+        Item[] items = {new Item(NONE_OF_THREE_NAMES, 8, -5)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(7));
+        assertThat(items[0].quality, is(-5));
+    }
+
+    @Test
+    public void doSomething_이름이셋다아니고_sellIn이8이고_quailty가25일때() {
+        Item[] items = {new Item(NONE_OF_THREE_NAMES, 8, 25)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(7));
+        assertThat(items[0].quality, is(24));
+    }
+
+    @Test
+    public void doSomething_이름이셋다아니고_sellIn이8이고_quailty가60일때() {
+        Item[] items = {new Item(NONE_OF_THREE_NAMES, 8, 60)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(7));
+        assertThat(items[0].quality, is(59));
+    }
+
+    @Test
+    public void doSomething_이름이셋다아니고_sellIn이13이고_quailty가음수일때() {
+        Item[] items = {new Item(NONE_OF_THREE_NAMES, 13, -5)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(12));
+        assertThat(items[0].quality, is(-5));
+    }
+
+    @Test
+    public void doSomething_이름이셋다아니고_sellIn이13이고_quailty가25일때() {
+        Item[] items = {new Item(NONE_OF_THREE_NAMES, 13, 25)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(12));
+        assertThat(items[0].quality, is(24));
+    }
+
+    @Test
+    public void doSomething_이름이셋다아니고_sellIn이13이고_quailty가60일때() {
+        Item[] items = {new Item(NONE_OF_THREE_NAMES, 13, 60)};
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.updateQuality();
+
+        assertThat(items[0].sellIn, is(12));
+        assertThat(items[0].quality, is(59));
+    }
 }
