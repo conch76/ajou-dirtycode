5064e9faa5d048e64bce1e8111940759c0bbb703
HongJow <hoongjow@gmail.com>
2019-07-24 16:55:05 +0900

    add testcode for Aged Brie item

--- src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
+++ src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
@@ -28,6 +28,42 @@ public class DirtySampleTest {
 
         assertThat(items[0].quality, is(3));
         assertThat(items[0].sellIn, is(-4));
-
     }
+
+    @Test
+    public void update_quality_Aged_Brie_has_enough_sellIn_and_quality_less_than_50(){
+        //quality: + 1, sellIn: - 1
+        Item[] items = new Item[1];
+        items[0] = new Item("Aged Brie", 5, 5);
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.update_quality();
+
+        assertThat(items[0].quality, is(6));
+        assertThat(items[0].sellIn, is(4));
+    }
+
+    @Test
+    public void update_quality_Aged_Brie_has_enough_sellIn_and_quality_more_than_50(){
+        //quality: + 0, sellIn: - 1
+        Item[] items = new Item[1];
+        items[0] = new Item("Aged Brie", 5, 55);
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.update_quality();
+
+        assertThat(items[0].quality, is(55));
+        assertThat(items[0].sellIn, is(4));
+    }
+
+    @Test
+    public void update_quality_Aged_Brie_has_negative_sellIn_and_quality_less_than_50(){
+        //quality: + 2, sellIn: - 1
+        Item[] items = new Item[1];
+        items[0] = new Item("Aged Brie", -3, 5);
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.update_quality();
+
+        assertThat(items[0].quality, is(7));
+        assertThat(items[0].sellIn, is(-4));
+    }
+
 }
