1e20ebd0002f28d7ea2d935ac08fbda46e99b4bb
HongJow <hoongjow@gmail.com>
2019-07-24 16:55:50 +0900

    add testcode for TAFKAL80ETC item

--- src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
+++ src/test/java/ac/kr/ajou/dirt/DirtySampleTest.java
@@ -66,4 +66,65 @@ public class DirtySampleTest {
         assertThat(items[0].sellIn, is(-4));
     }
 
+    @Test
+    public void update_quality_TAFKAL80ETC_has_sellIn_between_0_and_6_and_quality_less_than_50(){
+        //quality: + 3, sellIn: - 1
+        Item[] items = new Item[1];
+        items[0] = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 5);
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.update_quality();
+
+        assertThat(items[0].quality, is(8));
+        assertThat(items[0].sellIn, is(4));
+    }
+
+    @Test
+    public void update_quality_TAFKAL80ETC_has_sellIn_between_6_and_11_and_quality_less_than_50(){
+        //quality: + 2, sellIn: - 1
+        Item[] items = new Item[1];
+        items[0] = new Item("Backstage passes to a TAFKAL80ETC concert", 7, 5);
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.update_quality();
+
+        assertThat(items[0].quality, is(7));
+        assertThat(items[0].sellIn, is(6));
+    }
+
+    @Test
+    public void update_quality_TAFKAL80ETC_has_sellIn_more_than_11_and_quality_less_than_50(){
+        //quality: + 1, sellIn: - 1
+        Item[] items = new Item[1];
+        items[0] = new Item("Backstage passes to a TAFKAL80ETC concert", 12, 5);
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.update_quality();
+
+        assertThat(items[0].quality, is(6));
+        assertThat(items[0].sellIn, is(11));
+    }
+
+    @Test
+    public void update_quality_TAFKAL80ETC_has_quality_more_than_50(){
+        //quality: maintain, sellIn: - 1
+        Item[] items = new Item[1];
+        items[0] = new Item("Backstage passes to a TAFKAL80ETC concert", 7, 55);
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.update_quality();
+
+        assertThat(items[0].quality, is(55));
+        assertThat(items[0].sellIn, is(6));
+    }
+
+    @Test
+    public void update_quality_TAFKAL80ETC_has_negative_sellIn(){
+        //quality: equal 0, sellIn: - 1
+        Item[] items = new Item[1];
+        items[0] = new Item("Backstage passes to a TAFKAL80ETC concert", -2, 55);
+        DirtySample dirtySample = new DirtySample(items);
+        dirtySample.update_quality();
+
+        assertThat(items[0].quality, is(0));
+        assertThat(items[0].sellIn, is(-3));
+    }
+
+
 }
