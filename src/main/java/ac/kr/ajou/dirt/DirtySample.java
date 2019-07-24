3f3061290b2e7b54eda8e3bbe33bc36532f9096a
haejin <bae05030@gmail.com>
2019-07-24 17:34:41 +0900

    Extract method

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -15,41 +15,57 @@ class DirtySample {
             if (is_item_name_equal_test(item, SULFURAS_HAND_OF_RAGNAROS)) {
 
             } else if (is_item_name_equal_test(item, BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                if (item.quality < 50) {
-                    item.quality++;
-                    if (item_Sellln_Is_Under_Number(11,  item)) {
-                        item.quality++;
-                    }
-                    if (item_Sellln_Is_Under_Number(6, item)) {
-                        item.quality++;
-                    }
-                }
-                item.sellIn--;
-                if (item_Sellln_Is_Under_Number(0, item)) {
-                    item.quality = 0;
-                }
+                iten_name_is_BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT(item);
             } else if (is_item_name_equal_test(item, AGED_BRIE)) {
-                if (item.quality < 50) {
-                    item.quality++;
-                }
-                item.sellIn--;
-                if (item_Sellln_Is_Under_Number(0, item)) {
-                    if (item.quality < 50) {
-                        item.quality++;
-                    }
-                }
+                item_quality_name_is_AGED_BRIE(item);
             } else {
-                if (item.quality > 0) {
-                    item.quality--;
-                }
-                item.sellIn--;
-                if (item_Sellln_Is_Under_Number(0, item)) {
-                    if (item.quality > 0) {
-                        item.quality--;
-                    }
-                }
+                item_is_just_stuff(item);
             }
         }
+    }
+
+    private void item_is_just_stuff(Item item) {
+        item_quality_is_over_0(item);
+        update_sellln(item);
+        if (item_Sellln_Is_Under_Number(0, item)) {
+            item_quality_is_over_0(item);
+        }
+    }
+
+    private void item_quality_name_is_AGED_BRIE(Item item) {
+        item_quality_is_under_50(item, item.quality < 50);
+        update_sellln(item);
+        if (item_Sellln_Is_Under_Number(0, item)) {
+            item_quality_is_under_50(item, item.quality < 50);
+        }
+    }
+
+    private void iten_name_is_BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+            item_quality_is_under_50(item, item_Sellln_Is_Under_Number(11,  item));
+            item_quality_is_under_50(item, item_Sellln_Is_Under_Number(6, item));
+        }
+        update_sellln(item);
+        if (item_Sellln_Is_Under_Number(0, item)) {
+            item.quality = 0;
+        }
+    }
+
+    private void item_quality_is_over_0(Item item) {
+        if (item.quality > 0) {
+            item.quality--;
+        }
+    }
+
+    private void item_quality_is_under_50(Item item, boolean b) {
+        if (b) {
+            item.quality++;
+        }
+    }
+
+    private void update_sellln(Item item) {
+        item.sellIn--;
     }
 
     private boolean is_item_name_equal_test(Item item, String sulfurasHandOfRagnaros) {
