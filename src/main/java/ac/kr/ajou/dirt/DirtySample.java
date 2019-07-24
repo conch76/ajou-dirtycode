f2cd53df8cb22eb570418c16308ab9cab2245ea4
haejin <bae05030@gmail.com>
2019-07-24 17:18:04 +0900

    modify wrong code

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -78,9 +78,15 @@ class DirtySample {
                 if (item.sellIn < 0) {
                     item.quality = 0;
                 }
-            } else if (item.equals(AGED_BRIE)) {
+            } else if (item.name.equals(AGED_BRIE)) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
+                }
+                item.sellIn = item.sellIn - 1;
+                if (item.sellIn < 0) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
                 }
             } else {
                 if (item.quality > 0) {
