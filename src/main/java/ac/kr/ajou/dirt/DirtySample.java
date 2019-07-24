f3511407881211b6af7a5c2d1ea09043e0d4d8f6
haejin <bae05030@gmail.com>
2019-07-24 17:23:57 +0900

    simplify calculater code

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -16,36 +16,36 @@ class DirtySample {
 
             } else if (is_item_name_equal_test(item, BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                    item.quality++;
                     if (item_Sellln_Is_Under_Number(11,  item)) {
-                        item.quality = item.quality + 1;
+                        item.quality++;
                     }
                     if (item_Sellln_Is_Under_Number(6, item)) {
-                        item.quality = item.quality + 1;
+                        item.quality++;
                     }
                 }
-                item.sellIn = item.sellIn - 1;
+                item.sellIn--;
                 if (item_Sellln_Is_Under_Number(0, item)) {
                     item.quality = 0;
                 }
             } else if (is_item_name_equal_test(item, AGED_BRIE)) {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                    item.quality++;
                 }
-                item.sellIn = item.sellIn - 1;
+                item.sellIn--;
                 if (item_Sellln_Is_Under_Number(0, item)) {
                     if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+                        item.quality++;
                     }
                 }
             } else {
                 if (item.quality > 0) {
-                    item.quality = item.quality - 1;
+                    item.quality--;
                 }
-                item.sellIn = item.sellIn - 1;
+                item.sellIn--;
                 if (item_Sellln_Is_Under_Number(0, item)) {
                     if (item.quality > 0) {
-                        item.quality = item.quality - 1;
+                        item.quality--;
                     }
                 }
             }
