687e65bdf3ab2f928e43a5efe7fa443e5db2a7f2
kimdohyun <kdh5998@ajou.ac.kr>
2019-07-24 14:11:46 +0900

    파라미터의 i를 nth_count로, s를 name으로 다시 수정했습니다.

--- src/main/java/ac/kr/ajou/dirt/DirtySample.java
+++ src/main/java/ac/kr/ajou/dirt/DirtySample.java
@@ -10,21 +10,21 @@ public class DirtySample {
 
     public void updateQuality() {
 
-        for (int i = 0; i < items.length; i++) {
+        for (int nth_count = 0; nth_count < items.length; nth_count++) {
 
-            if(isSulfuras_HandOfRagnaros(items[i], "Sulfuras, Hand of Ragnaros"))
+            if(isSulfuras_HandOfRagnaros(items[nth_count], "Sulfuras, Hand of Ragnaros"))
                 continue;
 
-            MinusItemSellIn(i);
+            MinusItemSellIn(nth_count);
 
-            if (isAgedbrie(items[i], "Aged Brie")) {
-                updateQualityWhenIsAgedbrie(i);
+            if (isAgedbrie(items[nth_count], "Aged Brie")) {
+                updateQualityWhenIsAgedbrie(nth_count);
             }
-            else if(isBackstagePassesToATAFKAL80ETCConcert(items[i], "Backstage passes to a TAFKAL80ETC concert")){
-                updateQualityWhenIsBackstagePassesToATAFKAL80ETCConcert(i);
+            else if(isBackstagePassesToATAFKAL80ETCConcert(items[nth_count], "Backstage passes to a TAFKAL80ETC concert")){
+                updateQualityWhenIsBackstagePassesToATAFKAL80ETCConcert(nth_count);
             }
             else{
-                updateQualityWhenIsNeither(i);
+                updateQualityWhenIsNeither(nth_count);
             }
 
         }
@@ -32,59 +32,59 @@ public class DirtySample {
     }
 
 
-    private void updateQualityWhenIsNeither(int i) {
-        if (items[i].quality > 0 ) {
-            MinusItemQuality(i);
+    private void updateQualityWhenIsNeither(int nth_count) {
+        if (items[nth_count].quality > 0 ) {
+            MinusItemQuality(nth_count);
         }
-        if (items[i].sellIn < 0 && items[i].quality > 0)
-            MinusItemQuality(i);
+        if (items[nth_count].sellIn < 0 && items[nth_count].quality > 0)
+            MinusItemQuality(nth_count);
     }
 
-    private void updateQualityWhenIsBackstagePassesToATAFKAL80ETCConcert(int i) {
-        if (items[i].quality < 50) {
-            PlusItemQuality(i);
-            if (items[i].sellIn < 11 && items[i].quality < 50) {
-                PlusItemQuality(i);
+    private void updateQualityWhenIsBackstagePassesToATAFKAL80ETCConcert(int nth_count) {
+        if (items[nth_count].quality < 50) {
+            PlusItemQuality(nth_count);
+            if (items[nth_count].sellIn < 11 && items[nth_count].quality < 50) {
+                PlusItemQuality(nth_count);
             }
-            if (items[i].sellIn < 6 && items[i].quality < 50) {
-                PlusItemQuality(i);
+            if (items[nth_count].sellIn < 6 && items[nth_count].quality < 50) {
+                PlusItemQuality(nth_count);
             }
         }
-        if (items[i].sellIn < 0)
-            items[i].quality = 0;
+        if (items[nth_count].sellIn < 0)
+            items[nth_count].quality = 0;
     }
 
-    private void updateQualityWhenIsAgedbrie(int i) {
-        if (items[i].quality < 50) {
-            PlusItemQuality(i);
+    private void updateQualityWhenIsAgedbrie(int nth_count) {
+        if (items[nth_count].quality < 50) {
+            PlusItemQuality(nth_count);
         }
-        if (items[i].sellIn < 0 && items[i].quality < 50)
-            PlusItemQuality(i);
+        if (items[nth_count].sellIn < 0 && items[nth_count].quality < 50)
+            PlusItemQuality(nth_count);
     }
 
-    private boolean isBackstagePassesToATAFKAL80ETCConcert(Item item, String s) {
-        return item.name.equals(s);
+    private boolean isBackstagePassesToATAFKAL80ETCConcert(Item item, String name) {
+        return item.name.equals(name);
     }
 
-    private boolean isSulfuras_HandOfRagnaros(Item item, String s) {
-        return item.name.equals(s);
+    private boolean isSulfuras_HandOfRagnaros(Item item, String name) {
+        return item.name.equals(name);
     }
 
-    private boolean isAgedbrie(Item item, String s) {
-        return item.name.equals(s);
+    private boolean isAgedbrie(Item item, String name) {
+        return item.name.equals(name);
     }
 
 
-    private void MinusItemSellIn(int i) {
-        items[i].sellIn = items[i].sellIn - 1;
+    private void MinusItemSellIn(int nth_count) {
+        items[nth_count].sellIn = items[nth_count].sellIn - 1;
     }
 
-    private void MinusItemQuality(int i) {
-        items[i].quality = items[i].quality - 1;
+    private void MinusItemQuality(int nth_count) {
+        items[nth_count].quality = items[nth_count].quality - 1;
     }
 
-    private void PlusItemQuality(int i) {
-        items[i].quality = items[i].quality + 1;
+    private void PlusItemQuality(int nth_count) {
+        items[nth_count].quality = items[nth_count].quality + 1;
     }
 
 
